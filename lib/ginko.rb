dfe2920283db730805e4da339b3a55324a9e987f
Allan M <allanmukundi@hotmail.com>
2018-04-12 22:23:57 -0400

    Ginko runs code!

--- lib/ginko.rb
+++ lib/ginko.rb
@@ -1,24 +1,27 @@
 require 'discordrb'
 require_relative 'glot_api'
 
-bot = Discordrb::Bot.new(token: ENV['DISCORD_TOKEN'])
+bot = Discordrb::Bot.new(token: ENV['DISCORD_TOKEN']
 
 RUN_REGEX = %r{\s*(.+)\s+(.+)\s+(```\w+\n+|```)([\s\S]+)```}
 
 def languages(event)
   language_list = get_languages
-  resp = 'I am able to compile code written in:'
+  resp = 'I am able to run code written in:'
   language_list.each { |lang| resp += " #{lang}"}
   event.respond(resp)
 end
 
 def run_code(event, language, code)
-  event.respond(language)
-  event.respond(code)
+  begin
+    run(event, language, code)
+  rescue
+    event.respond(%Q{I cannot run code in #{language}, enter "@Ginko languages" to see my language options.})
+  end
 end
 
 def help_text
-    %Q{Hey, I'm Ginko, your robot code compiler. To use me properly,\
+    %Q{Hey, I'm Ginko, your robot code executor. To use me properly,\
     \nsimply mention me as follows:\n\n@Ginko <language>\n\\```[language for syntax highlighting]\n<code>\n\\```\n\
     \nFor a list of supported languages, send: "@Ginko languages".\
     \nFor a link to my GitHub repo, send: "@Ginko info".}
--- lib/glot_api.rb
+++ lib/glot_api.rb
@@ -5,7 +5,36 @@ def get_languages
   url = 'https://run.glot.io/languages'
   uri = URI(url)
 
-  response = Net::HTTP.get(uri)
-  data = JSON.parse(response)
-  languages = data.map { |lang| lang['name'] }
+  json_response = Net::HTTP.get(uri)
+  response = JSON.parse(json_response)
+  languages = response.map { |lang| lang['name'] }
+end
+
+def run(event, language, code)
+  url = "https://run.glot.io/languages/#{language}/latest"
+  uri = URI(url)
+
+  data = {
+    'files' => [{'name' => 'main', 'content' => code}]
+  }
+
+  https = Net::HTTP.new(uri.host, uri.port)
+  https.use_ssl = true
+
+  request = Net::HTTP::Post.new(uri)
+
+  request['Authorization'] = "Token #{ENV['GLOT_TOKEN']}"
+  request['Content-type'] = 'application/json'
+
+  request.body = data.to_json
+  response = JSON.parse(https.request(request).body)
+
+  stdout = response['stdout']
+  stderr = response['stderr']
+  error = response['error']
+
+  event.respond("STDOUT:\n```#{stdout}```") if stdout != ''
+  event.respond("STDERR:\n```#{stderr}```") if stderr != ''
+  event.respond("ERROR:\n```#{error}```") if error != ''
+
 end
